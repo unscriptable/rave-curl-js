2a6af479094f94c7e7d73645e41d107f13d88b5c
unscriptable <john@unscriptable.com>
2014-05-02 17:54:03 -0400

    Update to new rave-amd-plugins API and bump to version 0.1.0.

--- rave.js
+++ rave.js
@@ -7,6 +7,12 @@ var js = require('curl/plugin/js');
 exports.create = create;
 
 function create (context) {
+	var info = {
+		name: 'curl/plugin/js',
+		module: js
+	};
 	if (!context.amdPluginMap) context.amdPluginMap = {};
-	context.amdPluginMap.js = 'curl/plugin/js';
+	context.amdPluginMap['js']
+		= context.amdPluginMap['curl/plugin/js']
+		= info;
 }
